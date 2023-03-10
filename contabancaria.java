3a1b0a04ab89fa1d91152eae205caade99aec86a
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-10 11:43:22 +0000

    revisado

--- contabancaria.java
+++ contabancaria.java
@@ -24,13 +24,13 @@ public static void main(String[] args){
     cliente1.saldo = 4500;
 
     Conta cliente2 = new Conta();
-    cliente2.nome = "João"
-    cliente2.agencia = 39
+    cliente2.nome = "João";
+    cliente2.agencia = 39;
     cliente2.nconta = 78450;
     cliente2.saldo = 8500;
 
     Conta cliente3 = new Conta();
-    cliente3.nome = "Jéssica"
+    cliente3.nome = "Jéssica";
     cliente3.agencia = 3806;
     cliente3.nconta = 17845;
     cliente3.saldo = 5500;
