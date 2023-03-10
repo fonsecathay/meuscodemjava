1e5cd9b3dc22d745de0b47493d49c99dff03e94a
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-09 23:02:37 -0300

    explicação do cog

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+/*public static void : assinatura padrão do main
+public: Indica que o método é Público. 
+Pode ser chamado por qualquer outra classe
+static: Significa que o método main é um método estático. 
+Isso significa que ele é um método da classe e não dos objetos
+void : Significa que é um método que não retorna
+nenhum valor. Se fosse um método que retorna
+por exemplo um inteiro, teria um int no lugar do
+void.
+(String[] args): O método recebe como parâmetro um vetor de strings. Com o uso
+do args você pode inicializar sua aplicação parametrizando-a, ou
+seja, você pode definir inúmeros parâmetros de acordo com o
+ambiente que o usuário está rodando a aplicação.
+println: imprime na tela*/
