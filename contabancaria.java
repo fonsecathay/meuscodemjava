500210d0db497b5e8a2d103a7c3586057405a4bb
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-10 11:25:36 +0000

    slide pag 4

---
+++ contabancaria.java
@@ -0,0 +1,14 @@
+public static class Conta {
+    //Atributos
+    public String nome;
+    public int agencia;
+    public int nconta;
+    public float saldo;
+
+    public void saque(){
+        //método saque
+    }
+    public void transfer(){
+        //método transferência
+    }
+}
