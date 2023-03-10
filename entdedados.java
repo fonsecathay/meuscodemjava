f7bc0d088622ef4ae757d94dc07714ef0296489a
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-09 23:31:05 -0300

    entradada de dados

---
+++ entdedados.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        String nome;
+
+        System.out.println("Nome:");
+        nome = ler.next();
+        System.out.printf("Nome: %s", nome);
+    }
+}
