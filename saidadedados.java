6d8af56ea52377781d70588cd018b15d30039470
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-17 19:00:25 -0300

    println

--- saidadedados.java
+++ saidadedados.java
@@ -4,3 +4,10 @@ public class Main
 		System.out.printf("%s possui %d anos.\n", "Juliana", 10);
 	}
 }
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello \nWorld");
+	}
+}
