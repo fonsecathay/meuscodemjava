875b60dfd7675f9ec076eae57fc8e4eb88239d34
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-18 00:18:22 -0300

    Create prints.java

---
+++ prints.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+	   
+	   System.out.println("Texto com Enter");
+	   System.out.print("Texto sem Enter\n");
+	   System.out.printf("Formato dos números %d", 10);
+	}
+}
