1dab72db5604ec841c5118fc1b0ea5f9102163a0
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-18 11:26:34 -0300

    Update somacfuncoes.java

--- somacfuncoes.java
+++ somacfuncoes.java
@@ -16,3 +16,23 @@ public class Main
 	    System.out.printf("Soma: %d", soma);
 	}
 }
+//ou
+/*import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner(System.in);
+	    int a, b, soma;
+		System.out.println("Informe um número:");
+		a = ler.nextInt();
+		System.out.println("Informe um número:");
+		b = ler.nextInt();
+		soma = calculo(a,b);	
+	    System.out.printf("Soma: %d", soma);
+	}
+	public static int calculo(int a, int b){
+	    int soma;
+	    soma = a + b;
+	    return soma;
+	}
+}*/
