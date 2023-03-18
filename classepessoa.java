414396f1ea75f4a49668b08d31cce0a8a2ca213e
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-18 12:08:36 -0300

    Update classepessoa.java

--- classepessoa.java
+++ classepessoa.java
@@ -1,1 +1,36 @@
-/*/
+public class Main{
+    public static class Pessoa {
+        //Atributos
+        public String nome;
+        public int idade;
+        public float peso;
+        //Construtor de objetos
+        Conta(){
+            System.out.println("Objeto criado com sucesso!");
+        }
+        public void andar(){
+            //método andar
+        }
+        public void dormir(){
+        //método dormir
+    }
+}
+public static void main(String[] args){
+    Pessoa humanos1 = new Pessoa();
+    humano1.nome = "Juliana";
+    cliente1.agencia = 8974;
+    cliente1.saldo = 4500;
+
+    Conta cliente2 = new Conta();
+    cliente2.nome = "João";
+    cliente2.agencia = 39;
+    cliente2.nconta = 78450;
+    cliente2.saldo = 8500;
+
+    Conta cliente3 = new Conta();
+    cliente3.nome = "Jéssica";
+    cliente3.agencia = 3806;
+    cliente3.nconta = 17845;
+    cliente3.saldo = 5500;
+}
+}
