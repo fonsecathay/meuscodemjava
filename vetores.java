47209755cdb37246f063c147e8b993016344dc8d
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-18 11:31:03 -0300

    Create vetores.java

---
+++ vetores.java
@@ -0,0 +1,9 @@
+//Exemplo 1: Implementando o vetor para imprimir seu conteúdo na tela.
+public class Main
+{
+	public static void main(String[] args) {
+	    int vetor [] = {23, 0, 2, 6, 8, 10, 2, 1, 0, 4};
+	    for(int i =0; i<= 9; i++)
+		System.out.println(vetor[i]);
+	}
+}
