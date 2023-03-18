24255d3e0557c61a2b42f1ac15db2e5988ec68dc
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-18 11:33:12 -0300

    Create vetores2.java

---
+++ vetores2.java
@@ -0,0 +1,9 @@
+//Exemplo 2: Implementando o vetor para imprimir seu conteúdo e seu índice na tela.
+public class Main
+{
+	public static void main(String[] args) {
+	    int vetor [] = {23, 0, 2, 6, 8, 10, 2, 1, 0, 4};
+	    for(int i =0; i<= 9; i++)
+		System.out.printf("%d -> %d\n", i,vetor[i]);
+	}
+}
