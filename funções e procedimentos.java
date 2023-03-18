1346d19d8a8df192fbb7c205a9196509e5d427ac
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-18 10:20:10 -0300

    Create funções e procedimentos.java

---
+++ funções e procedimentos.java
@@ -0,0 +1,11 @@
+public class Main{
+ public static void main(String[] args){
+     imprimirTela(); //Chamando a função
+     }
+     public static void imprimirTela(){
+         System.out.println("Isso é uma função");
+         
+     }
+    
+} 
+
