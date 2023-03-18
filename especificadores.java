beef69664fecdf9420342d609750322c74b05aa5
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-18 00:20:14 -0300

    Update and rename saidadedados.java to especificadores.java

--- especificadores.java
+++ especificadores.java
@@ -4,10 +4,3 @@ public class Main
 		System.out.printf("%s possui %d anos.\n", "Juliana", 10);
 	}
 }
-
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello \nWorld");
-	}
-}
